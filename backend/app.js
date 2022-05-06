49be3cd0e479b322e49468af531292fedc4b6302
Julien Crutain <juju510@hotmail.fr>
2022-05-06 20:45:11 +0200

    installation of helmet and rateLimit for attack protection

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,19 @@ const mongoose = require('mongoose') //import du module mongoose
 const userRoutes = require("./routes/user"); // import du fichier routes user
 const userSauces = require('./routes/sauces'); // import du fichier routes des sauces
 const path = require('path');
+// importation des modules de protections
+const helmet = require("helmet");
+const rateLimit = require('express-rate-limit');
 
 
 require('dotenv').config();
+
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // limite chaque adresse IP à 100 requêtes par fenêtre  ici pour 15 minutes 
+	standardHeaders: true, // Informations sur la limite de taux de retour dans les en-têtes 'RateLimit-*'
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 // connexion a la base de données mongoDB
 mongoose.connect(`mongodb+srv://${process.env.ID_MONGO_DB}:${process.env.PWD_MONGO_DB}@cluster0.58bjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
@@ -18,6 +28,8 @@ mongoose.connect(`mongodb+srv://${process.env.ID_MONGO_DB}:${process.env.PWD_MON
 // Permet d'analyser le corps de la requête.
 app.use(express.json()); 
 
+app.use(helmet()); 
+app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
 // configuration des CORS doit être placé avant les routes de l'API
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
